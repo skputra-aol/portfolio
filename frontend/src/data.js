cb519d7d6ac07d8a5c8046b9058657d45330812e
skputra-aol <skputra2016@aol.com>
2022-11-07 17:43:57 +0700

    basic product img list

---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data= {
+    products: [
+        {
+            name: 'Adidas fit shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 250,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p2.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 65,
+            countInStock:5,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Nike Fit Pant',
+            slug: 'nike-fit-pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 15,
+            countInStock:5,
+            brand: 'Puma',
+            rating: 4,
+            numReviews: 10,
+            description: 'high quality product',
+        }
+    ]
+}
+export default data;
